7466914a3383de2c3543126fbec1d5d66c4691c2
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2023-12-15 20:21:21 +0100

    refactor: create files.py

--- imagescraper/__main__.py
+++ imagescraper/__main__.py
@@ -7,51 +7,13 @@ Read URLs for a text file, get the prompt and image URLs for that page
 and save them.
 """
 import sys
-from pathlib import Path
 
 import bs4
 
+from . import files
+
 from . import download, process
 from .config import FIREFOX_URLS_PATH, EDGE_URLS_PATH, HEADERS
-
-
-def _read_file(path: Path) -> list[str]:
-    """
-    Return all non-empty lines in a text file.
-    """
-    results = []
-
-    with open(path, "r", encoding="utf-8") as f:
-        for line in f:
-            line = line.strip()
-            if not line:
-                continue
-            results.append(line)
-
-    return results
-
-
-def _all_urls(firefox_path: Path, edge_path: Path) -> list[str]:
-    """
-    Read URLs from Firefox and Edge text files if they exist and return combined
-    URLs list.
-    """
-    assert (
-        firefox_path.exists() or not edge_path.exists()
-    ), "Unable to find either the Firefox or Edge files of URLs"
-
-    urls = []
-    if firefox_path.exists():
-        firefox_urls = _read_file(firefox_path)
-        assert firefox_urls, "Firefox URLs text file cannot be empty"
-        urls.extend(firefox_urls)
-
-    if edge_path.exists():
-        edge_urls = _read_file(edge_path)
-        assert edge_urls, "Edge URLs text file cannot be empty"
-        urls.extend(edge_urls)
-
-    return urls
 
 
 def download_for_creation_page(url: str, html: str):
@@ -75,7 +37,7 @@ def main(args: list[str]) -> None:
         url = args.pop(0)
         urls = [url]
     else:
-        urls = _all_urls(FIREFOX_URLS_PATH, EDGE_URLS_PATH)
+        urls = files.all_urls(FIREFOX_URLS_PATH, EDGE_URLS_PATH)
 
     print("GET HTML FOR CREATION PAGE URLS")
     html_content = download.get_html_for_urls(urls, HEADERS)
---
+++ imagescraper/files.py
@@ -0,0 +1,44 @@
+"""
+File processing module.
+"""
+
+from pathlib import Path
+
+
+def _read_file(path: Path) -> list[str]:
+    """
+    Return all non-empty lines in a text file.
+    """
+    results = []
+
+    with open(path, "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            results.append(line)
+
+    return results
+
+
+def all_urls(firefox_path: Path, edge_path: Path) -> list[str]:
+    """
+    Read URLs from Firefox and Edge text files if they exist and return combined
+    URLs list.
+    """
+    assert (
+        firefox_path.exists() or not edge_path.exists()
+    ), "Unable to find either the Firefox or Edge files of URLs"
+
+    urls = []
+    if firefox_path.exists():
+        firefox_urls = _read_file(firefox_path)
+        assert firefox_urls, "Firefox URLs text file cannot be empty"
+        urls.extend(firefox_urls)
+
+    if edge_path.exists():
+        edge_urls = _read_file(edge_path)
+        assert edge_urls, "Edge URLs text file cannot be empty"
+        urls.extend(edge_urls)
+
+    return urls
