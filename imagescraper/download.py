4ae6086a4b713cbf2fca3141527ab1beb0434828
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2023-12-15 18:46:42 +0100

    refactor: remove lib directory

--- imagescraper/download.py
+++ imagescraper/download.py
@@ -1,10 +1,43 @@
 """
 Download module.
 """
+import re
+import hashlib
+
 import requests
 
-from . import lib
-from .config import IMG_OUTPUT_PATH, TIMEOUT
+from .config import CREATION_DIR_NAME_MAX_LENGTH, IMG_OUTPUT_PATH, TIMEOUT
+
+
+def _slugify(value: str) -> str:
+    """
+    Convert a value to lowercase alphanumeric and hyphens in place of spaces.
+    """
+    value = value.replace(" ", "-")
+    value = re.sub(r"[^\w\s-]", "", value)
+    value = value.lower()
+
+    return value
+
+
+def _as_folder_name(title: str) -> str:
+    """
+    Make a folder name as a short title and hash.
+
+    The first part is a short slug form of the title to keep it readable.
+
+    The end is a hash based on the entire title so that it is unique but
+    every time you run the app for the same URL it will be the same. So we
+    can re-download with new app logic. Or choose to skip URLs we already
+    downloaded, so we can focus on new URLs or failed URLs.
+    """
+    title_slug = _slugify(title)
+    title_slug = title_slug[:CREATION_DIR_NAME_MAX_LENGTH]
+
+    hash_value = hashlib.sha1(title.encode("utf-8"))
+    hash_str = hash_value.hexdigest()[:8]
+
+    return f"{title_slug}-{hash_str}"
 
 
 def _get_html(url: str, headers: dict[str, str]) -> str:
@@ -38,7 +71,7 @@ def download_images(title: str, image_urls: list[str]) -> None:
     Download image URLs for a creation page to a folder and make a text file
     containing the prompt.
     """
-    folder_name = lib.as_folder_name(title)
+    folder_name = _as_folder_name(title)
     print("Folder name", folder_name)
 
     folder_path = IMG_OUTPUT_PATH / folder_name
