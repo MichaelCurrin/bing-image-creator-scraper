8ffe2fc1eeeb58a9d6aa1c11d4e0d4a152fbb4e1
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2023-12-15 18:42:25 +0100

    update config.py

--- imagescraper/config.py
+++ imagescraper/config.py
@@ -9,10 +9,10 @@ VAR_DIR = APP_DIR / Path("var")
 FIREFOX_URLS_PATH = VAR_DIR / "history_processed" / "firefox_urls.txt"
 IMG_OUTPUT_PATH = VAR_DIR / "creations"
 
-CREATION_DIR_NAME_MAX_LENGTH = 40
-TIMEOUT = 5
-
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101"
     " Firefox/117.0",
 }
+
+CREATION_DIR_NAME_MAX_LENGTH = 40
+TIMEOUT = 5
