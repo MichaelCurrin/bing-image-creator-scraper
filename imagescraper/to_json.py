b628f497a5f91b2cf868a31ce6f31c03b83d001c
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2024-04-08 20:25:09 +0200

    update to_json.py

--- imagescraper/to_json.py
+++ imagescraper/to_json.py
@@ -1,14 +1,19 @@
+"""
+To JSON module.
+
+Build a JSON file of data based on the images and metadata text files
+stored in creations. This can be used for a web app for browsing content.
+"""
+
 import json
 from pathlib import Path
+from typing import Optional
 
-from .config import IMG_OUTPUT_DIR
+from .config import IMG_OUTPUT_DIR, WEB_APP_DATA, VAR_DIR
 
 
 FolderData = dict[str, str | list[str]]
 Folders = list[FolderData]
-
-# TODO get the path of the script and the dir
-# and strip that out so the path starts with /var for each image
 
 
 def read_metadata(metadata_file: Path) -> tuple[str, str]:
@@ -26,47 +31,71 @@ def read_metadata(metadata_file: Path) -> tuple[str, str]:
     return url, description
 
 
+def get_image_files(folder: Path, base_path: Path) -> list[str]:
+    """
+    Get a list of image files in a given folder.
+
+    :param folder: Path to the folder containing images.
+    param base_path: This is used as the base, so the part on the left is removed.
+
+    :returns: List of image paths.
+    """
+    image_paths = folder.glob("*.png")
+    return [str(path.relative_to(base_path)) for path in image_paths]
+
+
+def get_folder_data(folder: Path) -> Optional[FolderData]:
+    """
+    Get info about a given creation folder.
+    """
+    if not folder.is_dir():
+        return None
+
+    metadata_file = folder / "metadata.txt"
+
+    if not metadata_file.exists():
+        return None
+
+    url, description = read_metadata(metadata_file)
+    image_files = get_image_files(folder, VAR_DIR)
+
+    folder_data: FolderData = {
+        "folderName": folder.name,
+        "url": url,
+        "description": description,
+        "images": image_files,
+    }
+
+    return folder_data
+
+
 def generate_folder_data(parent_folder: Path) -> Folders:
     """
     Get info on prompts and images for each folder in a given folder.
     """
-    folders = []
+    folders_data = [get_folder_data(folder) for folder in parent_folder.iterdir()]
 
-    for folder in parent_folder.iterdir():
-        if not folder.is_dir():
-            continue
-
-        metadata_file = folder / "metadata.txt"
-
-        if not metadata_file.exists():
-            continue
-
-        url, description = read_metadata(metadata_file)
-        image_paths = folder.glob("*.png")
-        image_files = [str(path) for path in image_paths]
-
-        folder_data: FolderData = {
-            "folderName": folder.name,
-            "url": url,
-            "description": description,
-            "images": image_files,
-        }
-
-        folders.append(folder_data)
-
-    return folders
+    return [f for f in folders_data if f]
 
 
-def write_json(data: Folders, output_file: Path):
+def write_json(data: list[dict], output_file: Path) -> None:
+    """
+    Write out given data as a JSON file.
+    """
     with open(output_file, "w", encoding="utf-8") as json_file:
         json.dump(data, json_file, indent=2)
 
 
-def main():
+def main() -> None:
+    """
+    Main command-line entry-point.
+    """
     parent_folder_path = IMG_OUTPUT_DIR
-    output_json_path = Path("output.json")
+    output_json_path = WEB_APP_DATA
 
     data_to_write = generate_folder_data(parent_folder_path)
+
+    print(f"Writing to: {output_json_path}")
     write_json(data_to_write, output_json_path)
 
 
